6ad92007538504db7f34b6d58c472af16546e270
LUVINA\luvanloi <luvanloi@luvina.net>
2021-03-25 14:56:16 +0700

    code calendar

---
+++ MCalendar/app/build/generated/source/buildConfig/debug/luvina/net/mcalendar/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package luvina.net.mcalendar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "luvina.net.mcalendar";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
